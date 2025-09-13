3565166730f29170f44b273e9a38a76ddaf99f5c
arifa-005 <ashireen22@gmail.com>
2025-09-14 01:21:45 +0530

    Update and rename HelloCI.java to HelloCI.java

--- HelloCI.java
+++ HelloCI.java
@@ -1,5 +1,6 @@
 public class HelloCI {
     public static void main(String[] args) {
-        System.out.println("Hello, Continuous Integration with Jenkins!");
+       System.out.println("CI/CD pipeline updated successfully!");
+
     }
 }
