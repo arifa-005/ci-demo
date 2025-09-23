956873771d41e5f69f72a46397bced81ee8617bb
arifa-005 <ashireen22@gmail.com>
2025-09-23 09:13:07 +0530

    Update HelloCI.java

--- HelloCI.java
+++ HelloCI.java
@@ -1,7 +1,7 @@
 public class HelloCI {
     public static void main(String[] args) {
        System.out.println("CI/CD pipeline updated successfully!");
-        //System.out.println("hello world!");
+        System.out.println("hello world from cse-b!");
         System.out.println("hiii");
         System.out.println("world!");
         
